261d4e88630d5aedfa4a0dc12c6d70e3164dd28d
Collier <acollier@acollier-mac01.local>
2017-05-01 14:22:00 -0700

    Misstyped presenters

--- app/presenters/hyrax/faculty_work_show_presenter.rb
+++ app/presenters/hyrax/faculty_work_show_presenter.rb
@@ -1,10 +1,6 @@
 module Hyrax
-  class ThesisShowPresenter < Hyrax::WorkShowPresenter
-    delegate :date_submitted, :handle, :college, :department, :university,
-             :degree_level, :degree_name, :abstract, :advisor, :committee_member,
-             :geographical_area, :time_period, :date_available, :date_copyright,
-             :date_issued, :sponsor, :rights_access,
-             :alternative_title, :statement_of_responsibility, to: :solr_document
+  class FacultyWorkShowPresenter < Hyrax::WorkShowPresenter
+    delegate :handle, :abstract, :date_available, :date_issued, :sponsor, to: :solr_document
 
   end
 end
